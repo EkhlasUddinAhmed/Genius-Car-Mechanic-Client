4dd1914c91c70de5634ef50494ca44bb5bb3c42e
unknown <matrixedu14@gmail.com>
2023-03-10 04:18:15 +0600

    Environment variable Used

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -2,24 +2,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_apiKey,
-//   authDomain: process.env.REACT_APP_authDomain,
-//   projectId: process.env.REACT_APP_projectId,
-//   storageBucket: process.env.REACT_APP_storageBucket,
-//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//   appId: process.env.REACT_APP_appId, 
-// };
-
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAXpfNfr_OQm6_0RGgsoL9opODU2Ra1f3c",
-  authDomain: "genius-car-mechanic-67c32.firebaseapp.com",
-  projectId: "genius-car-mechanic-67c32",
-  storageBucket: "genius-car-mechanic-67c32.appspot.com",
-  messagingSenderId: "860352861611",
-  appId: "1:860352861611:web:323a6d232545765a326eee"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId, 
 };
+
+
+
 
 
 
